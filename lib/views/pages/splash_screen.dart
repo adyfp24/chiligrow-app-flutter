d5212780435708f8da388c1f816169ddecae32b4
adyfp24 <adyfp24@gmail.com>
2024-04-17 09:57:54 +0700

    feat: adding after splash screen management

--- lib/views/pages/splash_screen.dart
+++ lib/views/pages/splash_screen.dart
@@ -3,8 +3,24 @@ part of 'page.dart';
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
+  Future<void> _toNextPage(BuildContext context) async {
+    final UserProvider userProvider =
+        Provider.of<UserProvider>(context, listen: false);
+    await Future.delayed(Duration(seconds: 2),);
+    final secureStorage = FlutterSecureStorage();
+    final token = await secureStorage.read(key: 'token');
+
+    if (token != null) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      Navigator.pushReplacementNamed(context, '/regist');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    _toNextPage(context);
+
     return Scaffold(
       backgroundColor: Color(0xFFE1FCF9),
       body: Center(
