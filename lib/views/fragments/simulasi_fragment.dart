16460668e1a5b6f26b8854762cfaa69ca3d44470
adyfp24 <adyfp24@gmail.com>
2024-05-10 00:07:43 +0700

    fix: create simulasi's view

--- lib/views/fragments/simulasi_fragment.dart
+++ lib/views/fragments/simulasi_fragment.dart
@@ -1,13 +1,19 @@
 part of 'fragment.dart';
 
-class SimulasiFragment extends StatelessWidget {
+class SimulasiFragment extends StatefulWidget {
   const SimulasiFragment({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    TextEditingController _lahanController = TextEditingController();
-    TextEditingController _bibitController = TextEditingController();
+  _SimulasiFragmentState createState() => _SimulasiFragmentState();
+}
 
+class _SimulasiFragmentState extends State<SimulasiFragment> {
+  String? _selectedBibit;
+  TextEditingController _lahanController = TextEditingController();
+  TextEditingController _bibitController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -30,15 +36,11 @@ class SimulasiFragment extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image(
-              image: AssetImage('assets/img/regist-img.png'),
-              height: 250,
-              width: 150,
-            ),
             Container(
               width: 300,
-              height: 230,
+              height: 255,
               decoration: BoxDecoration(
                   border: Border.all(style: BorderStyle.solid),
                   borderRadius: BorderRadius.circular(20)),
@@ -46,19 +48,21 @@ class SimulasiFragment extends StatelessWidget {
                 children: [
                   SizedBox(height: 20),
                   Container(
-                      height: 40,
-                      width: double.infinity,
-                      color: Color.fromRGBO(175, 245, 237, 1),
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Masukkan Data',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                      )),
+                    height: 40,
+                    width: double.infinity,
+                    color: Color.fromRGBO(175, 245, 237, 1),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Masukkan Data',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
                   Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30, vertical: 20),
                     child: Column(
                       children: [
                         TextField(
@@ -70,13 +74,29 @@ class SimulasiFragment extends StatelessWidget {
                               labelText: 'Luas lahan'),
                         ),
                         SizedBox(height: 20),
-                        TextField(
-                          controller: _bibitController,
+                        DropdownButtonFormField<String>(
+                          value: _selectedBibit,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _selectedBibit =
+                                  newValue; // Menyimpan nilai pilihan bibit
+                            });
+                          },
                           decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              labelText: 'Jenis bibit'),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            labelText: 'Jenis bibit',
+                          ),
+                          items: <String>[
+                            'Bibit Cabai Merah',
+                            'Bibit Cabai Rawit'
+                          ].map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
                         ),
                       ],
                     ),
@@ -84,7 +104,7 @@ class SimulasiFragment extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 25),
             TextButton(
               onPressed: () {
                 showDialog(
@@ -97,8 +117,8 @@ class SimulasiFragment extends StatelessWidget {
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Icon(Icons.nature), // Icon tanaman
-                  SizedBox(width: 8), // Jarak antara ikon dan teks
+                  Icon(Icons.nature),
+                  SizedBox(width: 8),
                   Text(
                     'Lihat',
                     style: TextStyle(
@@ -112,7 +132,6 @@ class SimulasiFragment extends StatelessWidget {
               style: TextButton.styleFrom(
                 elevation: 1,
                 backgroundColor: Color.fromRGBO(175, 245, 237, 1),
-                // primary: Colors.white,
                 padding: EdgeInsets.symmetric(vertical: 20, horizontal: 110),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12.0),
