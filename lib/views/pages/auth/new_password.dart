8082e73f6429595aedfb4ad900a85856b7e1910f
adyfp24 <adyfp24@gmail.com>
2024-05-19 15:56:57 +0700

    feat: reset password view

--- lib/views/pages/auth/new_password.dart
+++ lib/views/pages/auth/new_password.dart
@@ -1,9 +1,138 @@
 part of '../page.dart';
+
 class ResetPasswordPage extends StatelessWidget {
-  const ResetPasswordPage({ Key? key }) : super(key: key);
+  const ResetPasswordPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    TextEditingController _passController = TextEditingController();
+    TextEditingController _konfirmPassController = TextEditingController();
+
+    void _savePassword() {
+      String password = _passController.text;
+      String confirmPassword = _konfirmPassController.text;
+
+      if (password == confirmPassword) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Berhasil memperbarui kata sandi'),
+          ),
+        );
+        Navigator.pushNamed(context, '/login');
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Kata sandi dan konfirmasi kata sandi harus sama'),
+          ),
+        );
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Ubah Kata Sandi',
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        backgroundColor: Kgreen2,
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(50),
+            bottomRight: Radius.circular(50),
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            // Ganti homepageKey sesuai dengan konteks aplikasi Anda
+            Navigator.pop(context);
+          },
+        ),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Text(
+              'Masukkan kata sandi baru yang Anda inginkan',
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            Text(
+              'Kata Sandi Baru',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            TextField(
+              controller: _passController,
+              obscureText: true, // Menyembunyikan teks kata sandi
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                  labelText: 'Masukkan Kata Sandi Baru'),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            Text(
+              'Konfirmasi Kata Sandi Baru',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+            TextField(
+              controller: _konfirmPassController,
+              obscureText: true, // Menyembunyikan teks kata sandi
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                  labelText: 'Konfirmasi Ulang Kata Sandi'),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: TextButton(
+                onPressed: _savePassword,
+                child: Text(
+                  'Simpan',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Kgreen2,
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
